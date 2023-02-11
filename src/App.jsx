232ba69efd93638447dd4505e423b1200cf1086d
MDBossss <ferrari020203@gmail.com>
2023-02-11 11:20:35 +0100

    set right socket link

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import JoinRoom from './components/JoinRoom';
 import io from "socket.io-client";
 import { useState } from 'react';
 
-const socket = io.connect("https://tictactoe-vite-express-api-production.up.railway.app:5000");
+const socket = io.connect("tictactoe-vite-express-api-production.up.railway.app");
 
 
 function App() {
